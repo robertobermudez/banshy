76a9149c2a97aa6c48e855a74f981dcddbbab1ea
Roberto Bermudez <rbtobermudez@gmail.com>
2024-03-25 20:42:11 +0100

    Adding migrations for music files

--- migrations.rb
+++ migrations.rb
@@ -1,4 +1,4 @@
-class CreateVideoFileTable < ActiveRecord::Migration[5.2]
+class CreateVideoFileTable < ActiveRecord::Migration[7.1]
   def up
     unless ActiveRecord::Base.connection.table_exists? :video_files
       create_table :video_files do |table|
@@ -12,5 +12,22 @@ class CreateVideoFileTable < ActiveRecord::Migration[5.2]
 
   def down
     drop_table(:video_files) if ActiveRecord::Base.connection.table_exists?(:video_files)
+  end
+end
+
+class CreateMusicFileTable < ActiveRecord::Migration[7.1]
+  def up
+    unless ActiveRecord::Base.connection.table_exists? :music_files
+      create_table :music_files do |table|
+        table.string :name
+        table.string :path
+        table.string :duration
+        table.timestamps
+      end
+    end
+  end
+
+  def down
+    drop_table(:music_files) if ActiveRecord::Base.connection.table_exists?(:music_files)
   end
 end
