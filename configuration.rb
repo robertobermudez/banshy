c9e19dff049ccb8967b58f6e5d1fcda45c827b1e
Roberto Bermudez <rbtobermudez@gmail.com>
2024-04-03 23:35:27 +0200

    Refactor root path

--- configuration.rb
+++ configuration.rb
@@ -7,11 +7,13 @@ require 'active_record'
 require 'logger'
 require 'singleton'
 
+
 APP_ROOT_PATH = File.expand_path(__dir__).freeze
-PATH_FILES = '~/.banshy/'.freeze
+PATH_FILES = File.join(ENV['HOME'], '/.banshy/').freeze
 PATH_DB_TEST = File.join(APP_ROOT_PATH, 'test', 'datab.db').freeze
 RESOURCE_XML = File.join(APP_ROOT_PATH, 'resources', 'gresources.xml').freeze
 RESOURCE_BIN = File.join(APP_ROOT_PATH, 'gresource.bin').freeze
+$LOAD_PATH.unshift File.expand_path(APP_ROOT_PATH, 'lib/gems').freeze
 
 # Require all ruby files in the application folder recursively
 Dir[File.join(APP_ROOT_PATH, 'application/**', '*.rb')].each { |file| require file }
